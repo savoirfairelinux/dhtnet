a1d294f50b80565c767a77ba004545609e1007c1
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-07-17 22:42:13 -0400

    add turn cache
    Change-Id: I71ed970e4005d73035223ac8b32c24b27e923f1e

---
+++ include/turn_params.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (C) 2004-2023 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "ip_utils.h"
+#include <string>
+
+namespace dhtnet {
+
+struct TurnTransportParams
+{
+    dhtnet::IpAddr server;
+    std::string domain; // Used by cache_turn
+    // Plain Credentials
+    std::string realm;
+    std::string username;
+    std::string password;
+};
+
+}
---
+++ src/turn/turn_transport.h
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2004-2023 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "ip_utils.h"
+#include "turn_params.h"
+
+#include <opendht/logger.h>
+
+#include <functional>
+#include <memory>
+#include <string>
+
+namespace dht {
+namespace log {
+class Logger;
+}
+}
+
+namespace dhtnet {
+
+using Logger = dht::log::Logger;
+
+/**
+ * This class is used to test connection to TURN servers
+ * No other logic is implemented.
+ */
+class TurnTransport
+{
+public:
+    TurnTransport(const TurnTransportParams& param, std::function<void(bool)>&& cb, const std::shared_ptr<Logger>& logger = {});
+    ~TurnTransport();
+    void shutdown();
+
+private:
+    TurnTransport() = delete;
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+} // namespace jami
