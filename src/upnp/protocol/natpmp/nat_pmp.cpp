baa52d973881bdc0212cae2b816b5f80997e1ee6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2024-03-25 13:28:30 -0400

    natpmp: fix build on windows (no natpmp, no poll.h)
    Change-Id: I57afba75c41a4f46ec6f97d3ebd2b0a33151cddb

--- src/upnp/protocol/natpmp/nat_pmp.cpp
+++ src/upnp/protocol/natpmp/nat_pmp.cpp
@@ -15,9 +15,9 @@
  *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 #include "nat_pmp.h"
-#include <poll.h>
 
 #if HAVE_LIBNATPMP
+#include <poll.h>
 
 namespace dhtnet {
 namespace upnp {
