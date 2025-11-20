9061ed8388917a7ccf0fcaf051b23b75e975da76
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2025-11-20 14:21:24 -0500

    ChannelSocket: close on read error
    Change-Id: If9cf4622b7f521558f3b067dcc69d65b05d19c4f

--- include/generic_io.h
+++ include/generic_io.h
@@ -39,64 +39,86 @@ public:
 
     virtual ~GenericSocket() { shutdown(); }
 
+    /** 
+     * Receive callback type.
+     * @param buf Buffer containing received data
+     * @param len Length of the received data
+     * @return Number of bytes processed, or negative value to indicate an error and close the socket
+     */
     using RecvCb = std::function<ssize_t(const ValueType* buf, std::size_t len)>;
 
-    /// Close established connection
-    /// \note Terminate outstanding blocking read operations with an empty error code, but a 0 read size.
+    /**
+     * Close established connection.
+     * @note Terminate outstanding blocking read operations with an empty error code, but a 0 read size.
+     */
     virtual void shutdown() {}
 
-    /// Set Rx callback
-    /// \warning This method is here for backward compatibility
-    /// and because async IO are not implemented yet.
+    /**
+     * Set Rx callback.
+     * @warning This method is here for backward compatibility
+     * and because async IO are not implemented yet.
+     */
     virtual void setOnRecv(RecvCb&& cb) = 0;
 
     virtual bool isReliable() const = 0;
 
     virtual bool isInitiator() const = 0;
 
-    /// Return maximum application payload size.
-    /// This value is negative if the session is not ready to give a valid answer.
-    /// The value is 0 if such information is irrelevant for the session.
-    /// If stricly positive, the user must use send() with an input buffer size below or equals
-    /// to this value if it want to be sure that the transport sent it in an atomic way.
-    /// Example: in case of unreliable transport using packet oriented IO,
-    /// this value gives the maximal size used to send one packet.
+    /**
+     * Return maximum application payload size.
+     * This value is negative if the session is not ready to give a valid answer.
+     * The value is 0 if such information is irrelevant for the session.
+     * If stricly positive, the user must use send() with an input buffer size below or equals
+     * to this value if it want to be sure that the transport sent it in an atomic way.
+     * Example: in case of unreliable transport using packet oriented IO,
+     * this value gives the maximal size used to send one packet.
+     */
     virtual int maxPayload() const = 0;
 
-    /// Wait until data to read available, timeout or io error
-    /// \param ec error code set in case of error (if return value is < 0)
-    /// \return positive number if data ready for read, 0 in case of timeout or error.
-    /// \note error code is not set in case of timeout, but set only in case of io error
-    /// (i.e. socket deconnection).
-    /// \todo make a std::chrono version for the timeout
+    /**
+     * Wait until data to read available, timeout or io error.
+     * @param ec error code set in case of error (if return value is < 0)
+     * @return positive number if data ready for read, 0 in case of timeout or error.
+     * @note error code is not set in case of timeout, but set only in case of io error
+     * (i.e. socket deconnection).
+     * @todo make a std::chrono version for the timeout
+     */
     virtual int waitForData(std::chrono::milliseconds timeout, std::error_code& ec) const = 0;
 
-    /// Write a given amount of data.
-    /// \param buf data to write.
-    /// \param len number of bytes to write.
-    /// \param ec error code set in case of error.
-    /// \return number of bytes written, 0 is valid.
-    /// \warning error checking consists in checking if \a !ec is true, not if returned size is 0
-    /// as a write of 0 could be considered a valid operation.
+    /**
+     * Write a given amount of data.
+     * @param buf data to write.
+     * @param len number of bytes to write.
+     * @param ec error code set in case of error.
+     * @return number of bytes written, 0 is valid.
+     * @warning error checking consists in checking if \a !ec is true, not if returned size is 0
+     * as a write of 0 could be considered a valid operation.
+     */
     virtual std::size_t write(const ValueType* buf, std::size_t len, std::error_code& ec) = 0;
 
-    /// Read a given amount of data.
-    /// \param buf data to read.
-    /// \param len number of bytes to read.
-    /// \param ec error code set in case of error.
-    /// \return number of bytes read, 0 is valid.
-    /// \warning error checking consists in checking if \a !ec is true, not if returned size is 0
-    /// as a read of 0 could be considered a valid operation (i.e. non-blocking IO).
+    /**
+     * Read a given amount of data.
+     * @param buf data to read.
+     * @param len number of bytes to read.
+     * @param ec error code set in case of error.
+     * @return number of bytes read, 0 is valid.
+     * @warning error checking consists in checking if \a !ec is true, not if returned size is 0
+     * as a read of 0 could be considered a valid operation (i.e. non-blocking IO).
+     */
     virtual std::size_t read(ValueType* buf, std::size_t len, std::error_code& ec) = 0;
 
-    /// write() adaptor for STL containers
+    /**
+     * write() adaptor for STL containers.
+     */
     template<typename U>
     std::size_t write(const U& obj, std::error_code& ec)
     {
         return write(obj.data(), obj.size() * sizeof(typename U::value_type), ec);
     }
 
-    /// read() adaptor for STL containers
+    /**
+     * read() adaptor for STL containers.
+     */
     template<typename U>
     std::size_t read(U& storage, std::error_code& ec)
     {
@@ -106,14 +128,18 @@ public:
         return res;
     }
 
-    /// Return the local IP address if known.
-    /// \note The address is not valid (addr.isUnspecified() returns true) if it's not known
-    /// or not available.
+    /**
+     * Return the local IP address if known.
+     * @note The address is not valid (addr.isUnspecified() returns true) if it's not known
+     * or not available.
+     */
     virtual IpAddr localAddr() const { return {}; }
 
-    /// Return the remote IP address if known.
-    /// \note The address is not valid (addr.isUnspecified() returns true) if it's not known
-    /// or not available.
+    /**
+     * Return the remote IP address if known.
+     * @note The address is not valid (addr.isUnspecified() returns true) if it's not known
+     * or not available.
+     */
     virtual IpAddr remoteAddr() const { return {}; }
 
 protected:
--- src/channel_socket.cpp
+++ src/channel_socket.cpp
@@ -47,9 +47,33 @@ public:
 
     ~Impl() {}
 
+    bool stop()
+    {
+        if (isShutdown_.exchange(true))
+            return false;
+        if (shutdownCb_)
+            shutdownCb_(ec_shutdown_);
+        cv.notify_all();
+        if (rmFromMxSockCb_)
+            rmFromMxSockCb_();
+        return true;
+    }
+
+    std::error_code sendEOF()
+    {
+        if (auto ep = endpoint.lock()) {
+            std::error_code ec;
+            const uint8_t dummy = '\0';
+            ep->write(channel, &dummy, 0, ec);
+            return ec;
+        }
+        return std::make_error_code(std::errc::not_connected);
+    }
+
     ChannelReadyCb readyCb_ {};
     OnShutdownCb shutdownCb_ {};
     std::atomic_bool isShutdown_ {false};
+    std::error_code ec_shutdown_ {};
     const std::string name {};
     const uint16_t channel {};
     const std::weak_ptr<MultiplexedSocket> endpoint {};
@@ -91,7 +115,7 @@ ChannelSocketTest::deviceId() const
     return pimpl_deviceId;
 }
 
-std::string
+const std::string&
 ChannelSocketTest::name() const
 {
     return pimpl_name;
@@ -110,14 +134,13 @@ ChannelSocketTest::shutdown()
         std::unique_lock lk {mutex};
         if (!isShutdown_.exchange(true)) {
             lk.unlock();
-            shutdownCb_();
+            shutdownCb_(ec_shutdown_);
         }
         cv.notify_all();
     }
-
     if (auto peer = remote.lock()) {
         if (!peer->isShutdown_.exchange(true)) {
-            peer->shutdownCb_();
+            peer->shutdownCb_(ec_shutdown_);
         }
         peer->cv.notify_all();
     }
@@ -199,7 +222,7 @@ ChannelSocketTest::onShutdown(OnShutdownCb&& cb)
 
     if (isShutdown_) {
         lk.unlock();
-        shutdownCb_();
+        shutdownCb_(ec_shutdown_);
     }
 }
 
@@ -222,7 +245,7 @@ ChannelSocket::deviceId() const
     return {};
 }
 
-std::string
+const std::string&
 ChannelSocket::name() const
 {
     return pimpl_->name;
@@ -261,23 +284,37 @@ ChannelSocket::maxPayload() const
 void
 ChannelSocket::setOnRecv(RecvCb&& cb)
 {
-    std::lock_guard lkSockets(pimpl_->mutex);
+    std::unique_lock lkSockets(pimpl_->mutex);
     pimpl_->cb = std::move(cb);
     if (!pimpl_->buf.empty() && pimpl_->cb) {
-        pimpl_->cb(pimpl_->buf.data(), pimpl_->buf.size());
+        auto result = pimpl_->cb(pimpl_->buf.data(), pimpl_->buf.size());
         pimpl_->buf.clear();
+        if (result <= 0) {
+            pimpl_->ec_shutdown_ = result == 0 ? std::error_code()
+                                               : std::make_error_code(static_cast<std::errc>(-result));
+            pimpl_->stop();
+            lkSockets.unlock();
+            pimpl_->sendEOF();
+        }
     }
 }
 
 void
 ChannelSocket::onRecv(std::vector<uint8_t>&& pkt)
 {
-    std::lock_guard lkSockets(pimpl_->mutex);
+    std::unique_lock lkSockets(pimpl_->mutex);
     if (pimpl_->cb) {
-        pimpl_->cb(&pkt[0], pkt.size());
+        auto result = pimpl_->cb(pkt.data(), pkt.size());
+        if (result <= 0) {
+            pimpl_->ec_shutdown_ = result == 0 ? std::error_code()
+                                               : std::make_error_code(static_cast<std::errc>(-result));
+            pimpl_->stop();
+            lkSockets.unlock();
+            pimpl_->sendEOF();
+        }
         return;
     }
-    pimpl_->buf.insert(pimpl_->buf.end(), std::make_move_iterator(pkt.begin()), std::make_move_iterator(pkt.end()));
+    pimpl_->buf.insert(pimpl_->buf.end(), pkt.begin(), pkt.end());
     pimpl_->cv.notify_all();
 }
 
@@ -326,14 +363,7 @@ bool
 ChannelSocket::stop()
 {
     std::lock_guard lk {pimpl_->mutex};
-    if (pimpl_->isShutdown_.exchange(true))
-        return false;
-    if (pimpl_->shutdownCb_)
-        pimpl_->shutdownCb_();
-    pimpl_->cv.notify_all();
-    if (pimpl_->rmFromMxSockCb_)
-        pimpl_->rmFromMxSockCb_();
-    return true;
+    return pimpl_->stop();
 }
 
 void
@@ -341,11 +371,7 @@ ChannelSocket::shutdown()
 {
     if (!stop())
         return;
-    if (auto ep = pimpl_->endpoint.lock()) {
-        std::error_code ec;
-        const uint8_t dummy = '\0';
-        ep->write(pimpl_->channel, &dummy, 0, ec);
-    }
+    pimpl_->sendEOF();
 }
 
 std::size_t
@@ -400,7 +426,7 @@ ChannelSocket::onShutdown(OnShutdownCb&& cb)
     std::unique_lock lk {pimpl_->mutex};
     if (pimpl_->isShutdown_) {
         lk.unlock();
-        cb();
+        cb(pimpl_->ec_shutdown_);
     } else {
         pimpl_->shutdownCb_ = std::move(cb);
     }
