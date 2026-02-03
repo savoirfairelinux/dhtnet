5b2bb0b7aebdef17a4961accaf7374007c1febba
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2026-02-03 11:08:50 -0500

    tools: setCertificateStatus with certificate instead of id
    Change-Id: I92573fff71baf7a3c2d1d51693af1acc19dd1322

--- example/client.cpp
+++ example/client.cpp
@@ -51,16 +51,18 @@ client(dht::crypto::Identity id_client, dht::PkId id_server)
 int
 main(int argc, char** argv)
 {
+    if (argc < 2) {
+        fmt::print("Usage: {} <server_device_id>\n", argv[0]);
+        return 1;
+    }
+
     // Set the log level to 0 to avoids pj logs
     pj_log_set_level(0);
 
     // This is the root certificate that will be used to sign other certificates
     auto ca = dht::crypto::generateIdentity("ca_client");
-
     auto id_client = dht::crypto::generateIdentity("client", ca);
-
     auto id_server = dht::PkId(argv[1]);
-
     dhtnet::client(id_client, id_server);
 
     return 0;
