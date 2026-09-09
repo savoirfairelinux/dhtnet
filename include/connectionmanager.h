9d3e84e1a99a47f33a0c32a4fea416b63564ea89
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2026-09-09 16:32:45 -0400

    connection manager: change default legacyMode to Disabled
    Change-Id: Ie4a45934e72d8b4f87d81ee3c51c36514a448062

--- include/connectionmanager.h
+++ include/connectionmanager.h
@@ -337,7 +337,7 @@ struct ConnectionManager::Config
     /** Optional pseudo random generator to be used, allowing to control the seed. */
     std::unique_ptr<std::mt19937_64> rng;
 
-    LegacyMode legacyMode {LegacyMode::Enabled};
+    LegacyMode legacyMode {LegacyMode::Disabled};
 };
 
 } // namespace dhtnet
